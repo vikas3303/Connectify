06b118ae5af11a0264f1c41252fc7cfd88b16b58
vikas <vikassingh3303@gmail.com>
2024-12-16 19:32:34 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/message", messageRoutes);
 
 // ----------------------------------Deployment------------------------
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname1, "frontend", "build")));
 
   app.get("*", (req, res) => {
