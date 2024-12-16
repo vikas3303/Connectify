cc6b28bd6e9ca11d426b1e9b01a8a828b098a142
vikas <vikassingh3303@gmail.com>
2024-12-16 19:24:16 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,18 @@ app.use("/api/message", messageRoutes);
 
 // ----------------------------------Deployment------------------------
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  // });
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname1,"..", "frontend", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running successfully");
   });
 }
+
 // ---------------------------------Deployment-------------------------
 app.use(notFound);
 app.use(errorHandler);
