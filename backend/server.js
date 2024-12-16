b46577e3f9bf52cc140cb44274e5e0946b5bfa30
vikas <vikassingh3303@gmail.com>
2024-12-16 18:23:50 +0530

    fix  server.js

--- backend/server.js
+++ backend/server.js
@@ -17,25 +17,18 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // ----------------------------------Deployment------------------------
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.join(__dirname1, "../frontend/build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-const __dirname1 = path.resolve(__dirname, "..", "frontend", "build");
-app.use(express.static(__dirname1));
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname1, "../frontend/build/index.html"))
-);
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
 // ---------------------------------Deployment-------------------------
 app.use(notFound);
 app.use(errorHandler);
