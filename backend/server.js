e9a77f90d86c937ed68e92c68909f86533b66a5c
vikas <vikassingh3303@gmail.com>
2024-12-16 17:18:18 +0530

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,16 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // ----------------------------------Deployment------------------------
+const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  });
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname1, "../frontend/build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running successfully");
+    res.send("API is running..");
   });
 }
 // ---------------------------------Deployment-------------------------
